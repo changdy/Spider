196c51c8a77c7d44f7e57f546bd583b95f2c6c02
changdy <117969632@qq.com>
2018-03-08 22:44:29 +0800

    int array改名

--- pojo/src/main/java/com/smzdm/typehandler/IntArrayHandler.java
+++ pojo/src/main/java/com/smzdm/typehandler/IntArrayHandler.java
@@ -4,38 +4,37 @@ import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 
 import java.sql.*;
-import java.util.Optional;
 
 /**
  * Created by Changdy on 2018/1/27.
  */
 public class IntArrayHandler extends BaseTypeHandler<Integer[]> {
     @Override
-    public void setNonNullParameter(PreparedStatement preparedStatement, int i, Integer[] integers, JdbcType jdbcType) throws SQLException {
-        Connection conn = preparedStatement.getConnection();
-        Array array = conn.createArrayOf("integer", integers);
-        preparedStatement.setArray(i, array);
+    public void setNonNullParameter(PreparedStatement ps, int i, Integer[] parameter, JdbcType jdbcType) throws SQLException {
+        Connection conn = ps.getConnection();
+        Array array = conn.createArrayOf("integer", parameter);
+        ps.setArray(i, array);
     }
 
     @Override
-    public Integer[] getNullableResult(ResultSet resultSet, String s) throws SQLException {
-        return getArray(resultSet.getArray(s));
+    public Integer[] getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return convertToArray(rs.getArray(columnName));
     }
 
     @Override
-    public Integer[] getNullableResult(ResultSet resultSet, int i) throws SQLException {
-        return getArray(resultSet.getArray(i));
+    public Integer[] getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return convertToArray(rs.getArray(columnIndex));
     }
 
     @Override
-    public Integer[] getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
-        return getArray(callableStatement.getArray(i));
+    public Integer[] getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return convertToArray(cs.getArray(columnIndex));
     }
 
-    private Integer[] getArray(Array array) throws SQLException {
-        if (array == null) {
+    private Integer[] convertToArray(Array value) throws SQLException {
+        if (value == null) {
             return null;
         }
-        return (Integer[]) array.getArray();
+        return (Integer[]) value.getArray();
     }
 }
