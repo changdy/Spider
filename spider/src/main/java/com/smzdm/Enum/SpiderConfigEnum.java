7bfe268a77fb6ee304818b262fa48d3c3c3305eb
changdy <117969632@qq.com>
2018-03-09 00:52:58 +0800

    配置迁移

---
+++ spider/src/main/java/com/smzdm/Enum/SpiderConfigEnum.java
@@ -0,0 +1,36 @@
+package com.smzdm.Enum;
+
+/**
+ * Created by Changdy on 2018/3/8.
+ */
+public enum SpiderConfigEnum {
+    homeConfig(false, "timesort:home", "http://www.smzdm.com/homepage/json_more"),
+    latestConfig(true, "timesort:latest", "http://faxian.smzdm.com/json_more?page=2", "http://faxian.smzdm.com/json_more?page=1"),
+    hotUrl(true, null, "http://faxian.smzdm.com/json_more?filter=h3s0t0f0c0&page=5", "http://faxian.smzdm.com/json_more?filter=h3s0t0f0c0&page=4", "http://faxian.smzdm.com/json_more?filter=h3s0t0f0c0&page=3", "http://faxian.smzdm.com/json_more?filter=h3s0t0f0c0&page=2", "http://faxian.smzdm.com/json_more?filter=h3s0t0f0c0&page=1");
+
+
+    private String[] url;
+    private boolean discovery;
+    private String redisKey;
+
+    SpiderConfigEnum(boolean discovery, String redisKey, String... url) {
+        this.discovery = discovery;
+        this.redisKey = redisKey;
+        this.url = url;
+    }
+
+    public String[] getUrl() {
+        return url;
+    }
+
+
+    public boolean isDiscovery() {
+        return discovery;
+    }
+
+
+    public String getRedisKey() {
+        return redisKey;
+    }
+
+}
--- spider/src/main/java/com/smzdm/service/SpiderJobService.java
+++ spider/src/main/java/com/smzdm/service/SpiderJobService.java
@@ -2,12 +2,14 @@ package com.smzdm.service;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.smzdm.Enum.SpiderConfigEnum;
 import com.smzdm.basemapper.ArticleInfoMapper;
 import com.smzdm.basemapper.ArticleJsonMapper;
 import com.smzdm.basemapper.ArticleMapper;
 import com.smzdm.pojo.Article;
 import com.smzdm.pojo.ArticleInfo;
 import com.smzdm.pojo.ArticleJson;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import us.codecraft.webmagic.ResultItems;
@@ -15,22 +17,20 @@ import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 import javax.annotation.Resource;
+import java.lang.reflect.Array;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 @Service
 public class SpiderJobService {
 
     @Resource(name = "infoSpiderConfig")
     private PageProcessor infoSpiderConfig;
-    @Resource
-    private StringRedisTemplate stringRedisTemplate;
     @Resource
     private JsonConvertService jsonConvertService;
     @Resource
@@ -39,11 +39,14 @@ public class SpiderJobService {
     private ArticleInfoMapper articleInfoMapper;
     @Resource
     private ArticleJsonMapper articleJsonMapper;
+    @Resource
+    private RedisTemplate<String, Long> longValueTemplate;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
-    public void getInfo(String key, String[] url, boolean discovery) {
-        Long timeSort = Optional.ofNullable(stringRedisTemplate.opsForValue().get(key)).map(Long::valueOf).orElse(0L);
+    public void getInfo(SpiderConfigEnum spiderConfig) {
         Spider spider = Spider.create(infoSpiderConfig);
-        List<String> strings = Arrays.asList(url);
+        List<String> strings = Arrays.asList(spiderConfig.getUrl());
         List<ResultItems> resultItems = spider.getAll(strings);
         spider.close();
         JSONArray array = resultItems.stream().map(x -> (JSONArray) x.get("json")).reduce(new JSONArray(), (acc, element) -> {
@@ -51,34 +54,58 @@ public class SpiderJobService {
             return acc;
         });
         Stream<JSONObject> stream = array.stream().map(x -> (JSONObject) x).filter(x -> x.containsKey("article_id"));
+        generateArticleInfo(stream);
+        if (spiderConfig.getRedisKey() != null) {
+            insertArticle(spiderConfig, stream);
+        }
     }
 
+    //todo 各种插入数据库
+    private void generateArticleInfo(Stream<JSONObject> stream) {
+        LocalDateTime now = LocalDateTime.now();
+        List<ArticleInfo> infoList = stream.map(jsonConvertService::convertToInfo).filter(Objects::nonNull).collect(toList());
+        infoList.forEach(x -> x.setUpdateTime(now));
+    }
 
-    private List<Article> generateArticle(Stream<JSONObject> stream, boolean discovery, Long timeSort) {
+    private void insertArticle(SpiderConfigEnum spiderConfig, Stream<JSONObject> stream) {
+        String key = spiderConfig.getRedisKey();
+        boolean discovery = spiderConfig.isDiscovery();
+        long timeSort = Optional.ofNullable(longValueTemplate.opsForValue().get(key)).orElse(0L);
+        timeSort = generateArticle(stream, discovery, timeSort);
+        generateArticleJson(stream, discovery, timeSort);
+        longValueTemplate.opsForValue().set(key, timeSort);
+    }
+
+    private Long generateArticle(Stream<JSONObject> stream, boolean discovery, Long timeSort) {
         Stream<Article> articleStream = stream.filter(x -> x.getLong("timesort") > timeSort).map(jsonConvertService::convertToArticle).filter(Objects::nonNull);
         List<Integer> ids = articleStream.map(Article::getArticleId).collect(toList());
         articleMapper.deleteByIDList(ids);
         List<Article> articles = articleStream.collect(toList());
         articles.forEach(x -> x.setIsDiscovery(discovery));
-        return articles;
+        updateRedisType(articleStream, "article:mall", Article::getMall);
+        updateRedisType(articleStream, "article:channel", Article::getChannel);
+        updateRedisType(articleStream, "article:type", Article::getType);
+        updateRedisType(articleStream, "article:yh_type", Article::getYhType);
+        return articleStream.map(Article::getTimeSort).max(Long::compareTo).orElse(timeSort);
     }
 
-    private List<ArticleInfo> generateArticleInfo(Stream<JSONObject> stream) {
+    private void generateArticleJson(Stream<JSONObject> stream, boolean discovery, Long timeSort) {
         LocalDateTime now = LocalDateTime.now();
-        List<ArticleInfo> articleInfos = stream.map(jsonConvertService::convertToInfo).filter(Objects::nonNull).collect(toList());
-        articleInfos.forEach(x -> x.setUpdateTime(now));
-        return articleInfos;
-    }
-
-    private List<ArticleJson> generateArticleJson(Stream<JSONObject> stream, boolean discovery, Long timeSort) {
-        LocalDateTime now = LocalDateTime.now();
-        List<ArticleJson> jsons = stream.filter(x -> x.getLong("timesort") > timeSort).map(x -> {
+        List<ArticleJson> jsonList = stream.filter(x -> x.getLong("timesort") > timeSort).map(x -> {
             ArticleJson articleJson = new ArticleJson();
             articleJson.setContent(x.toJSONString());
             articleJson.setCreateDate(now);
             articleJson.setIsDiscovery(discovery);
             return articleJson;
         }).collect(toList());
-        return jsons;
+    }
+
+    private void updateRedisType(Stream<Article> articleStream, String key, Function<Article, String> function) {
+        Set<String> redisMembers = stringRedisTemplate.opsForSet().members(key);
+        List<String> streamMembers = articleStream.map(function).distinct().collect(toList());
+        streamMembers.removeAll(redisMembers);
+        if (streamMembers.size() > 0) {
+            stringRedisTemplate.opsForSet().add("key", (String[]) streamMembers.toArray());
+        }
     }
 }
