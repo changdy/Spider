42daa4b71768ff7fc8dfb6d170d36615d64eabb4
changdy <117969632@qq.com>
2018-03-17 19:53:15 +0800

    修复配置

--- spider/src/test/java/ScriptApplicationTests.java
+++ spider/src/test/java/ScriptApplicationTests.java
@@ -1,11 +1,29 @@
 
 import com.smzdm.StartApplication;
+import com.smzdm.mapper.CategoryMapper;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = StartApplication.class)
 public class ScriptApplicationTests {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private CategoryMapper mapper;
+
+    @Test
+    public void test() {
+        Map<String, String> map = new HashMap<>();
+        mapper.getCategoryMap().forEach(x -> map.put((String) x.get("title"), String.valueOf((Integer) x.get("id"))));
+        stringRedisTemplate.opsForHash().putAll("test", map);
+    }
 }
