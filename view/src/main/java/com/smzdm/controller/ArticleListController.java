6c7ea4a500deed7853c6b4afc4c2c469a5badb14
changdy <117969632@qq.com>
2018-05-24 22:27:14 +0800

    忘了 return了

--- view/src/main/java/com/smzdm/controller/ArticleListController.java
+++ view/src/main/java/com/smzdm/controller/ArticleListController.java
@@ -2,10 +2,8 @@ package com.smzdm.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.smzdm.mapper.ArticleMapper;
 import com.smzdm.model.ArticleModel;
-import com.smzdm.model.ArticleSearch;
 import com.smzdm.model.ResponseResult;
 import com.smzdm.model.SearchType;
 import com.smzdm.util.ResultUtil;
@@ -21,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
