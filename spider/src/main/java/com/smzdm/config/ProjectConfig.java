12fef268e8ded6b62449a8a61280893ce173b0ff
changdy <changdy@163.com>
2018-07-05 13:01:13 +0800

    增加redis 通知

--- spider/src/main/java/com/smzdm/config/ProjectConfig.java
+++ spider/src/main/java/com/smzdm/config/ProjectConfig.java
@@ -3,21 +3,34 @@ package com.smzdm.config;
 import com.smzdm.mapper.CategoryMapper;
 import com.smzdm.pojo.ArticleSubscription;
 import com.smzdm.pojo.Category;
+import com.smzdm.service.TopicMessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 public class ProjectConfig {
 
     @Autowired
     private CategoryMapper categoryMapper;
+    @Autowired
+    private RedisConfig redisConfig;
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Autowired
+    private TopicMessageListener messageListener;
 
     @Bean
     public List<Category> categories() {
@@ -34,5 +47,29 @@ public class ProjectConfig {
     @Bean
     public ValueOperations<String, String> valueOperations(StringRedisTemplate stringRedisTemplate) {
         return stringRedisTemplate.opsForValue();
+    }
+
+    @Bean
+    public RedisMessageListenerContainer configRedisMessageListenerContainer() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(redisConfig.getCorePoolSize());
+        executor.setMaxPoolSize(redisConfig.getMaxPoolSize());
+        executor.setQueueCapacity(redisConfig.getQueueCapacity());
+        executor.setKeepAliveSeconds(redisConfig.getKeepAliveSeconds());
+        executor.setThreadNamePrefix(redisConfig.getThreadNamePrefix());
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是由调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        // 设置Redis的连接工厂
+        container.setConnectionFactory(redisTemplate.getConnectionFactory());
+        // 设置监听使用的线程池
+        container.setTaskExecutor(executor);
+        // 设置监听的Topic
+        ChannelTopic channelTopic = new ChannelTopic(redisConfig.getExpiredTopic());
+        // 设置监听器
+        container.addMessageListener(messageListener, channelTopic);
+        return container;
     }
 }
---
+++ spider/src/main/java/com/smzdm/config/RedisConfig.java
@@ -0,0 +1,20 @@
+package com.smzdm.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Changdy on 2018/4/14.
+ */
+@Data
+@Component
+@ConfigurationProperties("redis-config")
+public class RedisConfig {
+    private String expiredTopic;
+    private Integer corePoolSize;
+    private Integer maxPoolSize;
+    private Integer queueCapacity;
+    private Integer keepAliveSeconds;
+    private String threadNamePrefix;
+}
